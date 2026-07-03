a765f721d3765e7677a0afc5ae53a7e122b91ec5
pooja waykar <waykarpooja2002@gmail.com>
2026-07-03 18:24:42 +0530

    react icon remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 // import BillingApp from './component/BillingApp'
 import BillingApplication from './BillingApplication'
